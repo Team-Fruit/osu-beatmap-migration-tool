24464d02c99f896b2073ec9fbaaeb3c19a5714c4
sjcl <jrhigasinihonn.0601@gmail.com>
2019-04-11 15:31:50 +0900

    Refactor

--- export.py
+++ export.py
@@ -2,11 +2,9 @@ import glob
 import json
 
 
-def main():
+if __name__ == '__main__':
     songs = [id for id in map(lambda p: p.split(' ')[0], glob.iglob('*/')) if id.isdigit()]
 
     with open('beatmaps.json', 'w') as f:
         json.dump(songs, f, ensure_ascii=False)
-
-if __name__ == '__main__':
-    main()
+        
