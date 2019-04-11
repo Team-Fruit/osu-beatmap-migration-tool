a7de9a96306ed635cec52d2ac993646d6c68a423
sjcl <jrhigasinihonn.0601@gmail.com>
2019-04-11 21:07:43 +0900

    Implement downloader with asyncio

--- osu_beatmap_downloader.py
+++ osu_beatmap_downloader.py
@@ -1,91 +1,109 @@
 import json
+import aiohttp
+import asyncio
 import re
-import glob
 import shutil
-import os
 from argparse import ArgumentParser
-from multiprocessing import Pool, freeze_support, RLock, Manager
-from functools import partial
-from requests import session
 from tqdm import tqdm
 
+
+class PositionManager:
+
+    def __init__(self, size):
+        self.size = size
+        self.dict = {}
+
+    def get_position(self, id):
+        if id in self.dict:
+            return self.dict[id]
+
+        positions = self.dict.values()
+        position = next(filter(lambda x: x not in positions, range(1, self.size+1)))
+        self.dict[id] = position
+        return position
+
+    def done(self, id):
+        del self.dict[id]
 
 def pbar_desc_size():
     return str(int(shutil.get_terminal_size().columns/4))
 
-def download(session, pids, id):
-    req = session.get('https://osu.ppy.sh/d/' + id, stream=True)
-    
-    if 'Content-Disposition' not in req.headers:
-        return 'Beatmap {} could not be found will be skipped'.format(id)
+async def download(session, position_manager, url):
+    async with session.get(url) as res:
+        if "Content-Disposition" not in res.headers:
+            tqdm.write("Beatmap {} could not be found will be skipped".format(id))
 
-    osz = req.headers['Content-Disposition'][21:-2]
-    osz = re.sub(r'(?:["*:<>?\\]|\/|\|)', ' ', osz)
-    desc = osz[osz.find(' ')+1:-4]
-    size = int(req.headers['Content-Length'])
+        osz = res.headers["Content-Disposition"][21:-2]
+        osz = re.sub(r'(?:["*:<>?\\]|\/|\|)', ' ', osz)
+        desc = osz[osz.find(' ')+1:-4]
+        size = int(res.headers['Content-Length'])
 
-    pid = os.getpid()
-    if pid not in pids:
-        pids.append(pid)
-    position = pids.index(pid)+1
-
-    desc_size = pbar_desc_size()
-    progress = tqdm(
-            total=size,
-            unit='B',
-            unit_scale=True,
-            position=position,
-            desc=desc,
-            ascii=True,
-            bar_format='{desc:'+desc_size+'.'+desc_size+'}{percentage:3.0f}%|{bar}{r_bar}')
-    with open(osz, 'wb') as o:
-        for chunk in req.iter_content(chunk_size=64 * 1024):
-            if chunk:
+        desc_size = pbar_desc_size()
+        progress = tqdm(
+                total=size,
+                unit='B',
+                unit_scale=True,
+                desc=desc,
+                ascii=True,
+                position=position_manager.get_position(url),
+                bar_format='{desc:'+desc_size+'.'+desc_size+'}{percentage:3.0f}%|{bar}{r_bar}')
+        
+        with open(osz, "wb") as o:
+            while True:
+                chunk = await res.content.read(4 * 1024)
+                if not chunk:
+                    break
                 o.write(chunk)
-                o.flush()
                 progress.update(len(chunk))
-        progress.close()
+            progress.close()
+            position_manager.done(url)
+
+async def parallel_download(username, password, ids, limit=4):
+    async with aiohttp.ClientSession() as session:
+        data = {
+            "action": "login",
+            "username": username,
+            "password": password,
+            "redirect": "index.php",
+            "sid": "",
+            "login": "Login"
+        }
+        async with session.post("https://osu.ppy.sh/forum/ucp.php", data=data) as res:
+            sem = asyncio.Semaphore(limit)
+
+            async def limited_download(session, position_manager, url):
+                with await sem:
+                    return await download(session, position_manager, url)
+
+            position_manager = PositionManager(limit)
+            tasks = [limited_download(session, position_manager, "https://osu.ppy.sh/d/" + id) for id in ids]
+
+            desc_size = pbar_desc_size()
+            for f in tqdm(asyncio.as_completed(tasks),
+                    total=len(ids),
+                    bar_format='{desc:'+desc_size+'.'+desc_size+'}{percentage:3.0f}%|{bar}{r_bar}',
+                    desc='Overall',
+                    position=0):
+                await f
 
 def main():
-    parser = ArgumentParser(description='osu! beatmap downloader')
+    parser = ArgumentParser(description="osu! beatmap downloader")
 
-    parser.add_argument('username', help='osu! account username')
-    parser.add_argument('password', help='osu! account password')
-    parser.add_argument('-s', '--skip', help='Skip if beatmap file already exists', action='store_true')
+    parser.add_argument("username", help="osu! account username")
+    parser.add_argument("password", help="osu! account password")
+    parser.add_argument("-s", "--skip", help="Skip if beatmap file already exists", action="store_true")
 
     args = parser.parse_args()
 
-    with open('beatmaps.json', 'r') as f:
+    with open("beatmaps.json", "r") as f:
         ids = json.load(f)
 
     if args.skip:
-        for id in map(lambda path: path.split(' ')[0], glob.iglob('*.osz')):
+        for id in map(lambda path: path.split(" ")[0], glob.iglob("*.osz")):
             ids.remove(id)
 
-    with session() as s:
-        para = {
-            'action': 'login',
-            'username': args.username,
-            'password': args.password,
-            'redirect': 'index.php',
-            'sid': '',
-            'login': 'Login'
-        }
-        res = s.post('https://osu.ppy.sh/forum/ucp.php', data=para)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(parallel_download(args.username, args.password, ids))
 
-        freeze_support()
-        with Pool(4, initializer=tqdm.set_lock, initargs=(RLock(),)) as p:
-            with Manager() as manager:
-                desc_size = pbar_desc_size()
-                for c in tqdm(
-                        p.imap_unordered(partial(download, s, manager.list()), ids),
-                        total=len(ids),
-                        bar_format='{desc:'+desc_size+'.'+desc_size+'}{percentage:3.0f}%|{bar}{r_bar}',
-                        desc='Overall',
-                        position=0):
-                    if c:
-                        tqdm.write(c)
-                    pass    
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
