25ca87009eb51b1f57987966fbf85c3f4f48fdb2
sjcl <jrhigasinihonn.0601@gmail.com>
2019-04-11 20:58:58 +0900

    Fix beatmap duplication

--- export.py
+++ export.py
@@ -3,7 +3,7 @@ import json
 
 
 if __name__ == '__main__':
-    songs = [id for id in map(lambda p: p.split(' ')[0], glob.iglob('*/')) if id.isdigit()]
+    songs = list(set([id for id in map(lambda p: p.split(' ')[0], glob.iglob('*/')) if id.isdigit()]))
 
     with open('beatmaps.json', 'w') as f:
         json.dump(songs, f, ensure_ascii=False)
