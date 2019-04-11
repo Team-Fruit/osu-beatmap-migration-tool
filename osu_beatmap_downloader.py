f26ced37eab83ab3b490e63fffdd260f551b7454
sjcl <jrhigasinihonn.0601@gmail.com>
2019-04-11 15:17:00 +0900

    Add download progress bar

--- osu_beatmap_downloader.py
+++ osu_beatmap_downloader.py
@@ -1,36 +1,58 @@
-import argparse
-import sys
 import json
 import re
 import glob
-from multiprocessing import Pool
+import shutil
+import os
+from argparse import ArgumentParser
+from multiprocessing import Pool, freeze_support, RLock, Manager
 from functools import partial
 from requests import session
+from tqdm import tqdm
 
 
-parser = argparse.ArgumentParser(description='osu! beatmap downloader')
+parser = ArgumentParser(description='osu! beatmap downloader')
 
 parser.add_argument('username', help='osu! account username')
 parser.add_argument('password', help='osu! account password')
 parser.add_argument('-s', '--skip', help='Skip if beatmap file already exists', action='store_true')
 
-def download(id, session):
+def pbar_desc_size():
+    return str(int(shutil.get_terminal_size().columns/4))
+
+def download(session, pids, id):
     req = session.get('https://osu.ppy.sh/d/' + id, stream=True)
     
     if 'Content-Disposition' not in req.headers:
-        print('Beatmap %s could not be found will be skipped' % id)
-        return
+        return 'Beatmap {} could not be found will be skipped'.format(id)
 
     osz = req.headers['Content-Disposition'][21:-2]
-    print(osz)
     osz = re.sub(r'(?:["*:<>?\\]|\/|\|)', ' ', osz)
+    desc = osz[osz.find(' ')+1:-4]
+    size = int(req.headers['Content-Length'])
+
+    pid = os.getpid()
+    if pid not in pids:
+        pids.append(pid)
+    position = pids.index(pid)+1
+
+    desc_size = pbar_desc_size()
+    progress = tqdm(
+            total=size,
+            unit='B',
+            unit_scale=True,
+            position=position,
+            desc=desc,
+            ascii=True,
+            bar_format='{desc:'+desc_size+'.'+desc_size+'}{percentage:3.0f}%|{bar}{r_bar}')
     with open(osz, 'wb') as o:
         for chunk in req.iter_content(chunk_size=64 * 1024):
             if chunk:
                 o.write(chunk)
                 o.flush()
+                progress.update(len(chunk))
+        progress.close()
 
-def main():
+if __name__ == '__main__':
     args = parser.parse_args()
 
     with open('beatmaps.json', 'r') as f:
@@ -51,8 +73,16 @@ def main():
         }
         res = s.post('https://osu.ppy.sh/forum/ucp.php', data=para)
 
-        with Pool(4) as p:
-            p.map(partial(download, session=s), ids)       
-
-if __name__ == '__main__':
-    main()
+        freeze_support()
+        with Pool(4, initializer=tqdm.set_lock, initargs=(RLock(),)) as p:
+            with Manager() as manager:
+                desc_size = pbar_desc_size()
+                for c in tqdm(
+                        p.imap_unordered(partial(download, s, manager.list()), ids),
+                        total=len(ids),
+                        bar_format='{desc:'+desc_size+'.'+desc_size+'}{percentage:3.0f}%|{bar}{r_bar}',
+                        desc='Overall',
+                        position=0):
+                    if c:
+                        tqdm.write(c)
+                    pass    
